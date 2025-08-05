eafd861f977b21716ce0e6417c52b263a7fcf231
Omar Mohammad <omarmd425@gmail.com>
2025-08-06 00:06:42 +0530

    generate html for the order summery

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+    productId: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    quantity: 2,
+}, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+}];
 
 export function addToCart(productId) {
   let matchingItem;
