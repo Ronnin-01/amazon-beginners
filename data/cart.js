b7f905b4c9059ebf84ed893faa238f441d8d37e4
Omar Mohammad <omarmd425@gmail.com>
2025-08-06 18:28:39 +0530

    delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,26 +1,39 @@
-export const cart = [{
+export let cart = [
+  {
     productId: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
     quantity: 2,
-}, {
+  },
+  {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1,
-}];
+  },
+];
 
 export function addToCart(productId) {
   let matchingItem;
-    cart.forEach((cartItem)=> {
-        if (productId === cartItem.productId) {
-            matchingItem = cartItem;
-            return;
-        }
-    });
-
-    if(matchingItem){
-      matchingItem.quantity += 1;
-    }else{
-        cart.push({
-        productId: productId,
-        quantity: 1,
-      });
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+      return;
     }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
 }
