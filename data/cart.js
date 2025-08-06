235a8cbb69bc9b2503ba9b50264bfdb9aedb79bd
Omar Mohammad <omarmd425@gmail.com>
2025-08-06 20:13:13 +0530

    save data and genetrate html for delivery options

--- data/cart.js
+++ data/cart.js
@@ -5,10 +5,12 @@ if (!cart) {
     {
       productId: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
       quantity: 2,
+      deliveryOptionId: "1",
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
+      deliveryOptionId: "2",
     },
   ];
 }
@@ -32,6 +34,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: "1",
     });
   }
   saveToStorage();
